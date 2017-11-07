767ac3f62840558a4570ad5b5b41b087d362655b
AhmedAbdelfattah0 <33193362+AhmedAbdelfattah0@users.noreply.github.com>
2017-11-08 00:51:42 +0200

    add language service to localize the view

---
+++ phones/views/js/languageService.js
@@ -0,0 +1,115 @@
+﻿(function () {
+    'use strict';
+    //language service to localize the view 
+    angular
+        .module('myApp')
+        .factory('languageService', function () {
+        var res = {
+            'phoneGallery': {
+                'eng-us': 'Phone Shope',
+                'arabic-eg': 'متجر الهواتف'
+
+            },
+            'type': {
+                'eng-us': 'Type',
+                'arabic-eg': 'النوع'
+
+            },
+            'home': {
+                'eng-us': 'Language',
+                'arabic-eg': 'اللغة'
+
+            },
+            'language': {
+                'eng-us': 'English',
+                'arabic-eg': 'العربية'
+
+            },
+            'filter': {
+                'eng-us': 'Filter by',
+                'arabic-eg': 'البحث بواسطة'
+            },
+            'prices': {
+                'eng-us': 'Prices on',
+                'arabic-eg': 'الاسعار بـ'
+
+            },
+            'USD': {
+                'eng-us': 'USD',
+                'arabic-eg': 'دولار'
+
+            }
+            ,
+            'EGP': {
+                'eng-us': 'EGP',
+                'arabic-eg': 'جنيه'
+
+            },
+            'Price': {
+                'eng-us': 'Price',
+                'arabic-eg': 'السعر'
+
+            }
+            ,
+            'Min': {
+                'eng-us': 'Min',
+                'arabic-eg': 'اقل'
+
+            }
+            ,
+            'Max': {
+                'eng-us': 'Max',
+                'arabic-eg': 'أكثر'
+
+            }
+            ,
+            'Color': {
+                'eng-us': 'Color',
+                'arabic-eg': 'اللون'
+
+            },
+            'Black': {
+                'eng-us': 'Black',
+                'arabic-eg': 'اسود'
+
+            },
+            'Gray': {
+                'eng-us': 'Gray',
+                'arabic-eg': 'رمادى'
+
+            },
+            'Gold': {
+                'eng-us': 'Gold',
+                'arabic-eg': 'ذهبى'
+
+            },
+            'Tablet': {
+                'eng-us': 'Tablet',
+                'arabic-eg': 'تابلت'
+
+            },
+            'Phone': {
+                'eng-us': 'Phone',
+                'arabic-eg': 'هاتف'
+
+            },
+            'Next': {
+                'eng-us': 'Next',
+                'arabic-eg': 'التالى'
+
+            },
+            'Prev': {
+                'eng-us': 'Prev',
+                'arabic-eg': 'السابق'
+
+            }
+        }
+
+        var currLang = /lang-ar/.test(window.top.location.href) ? 'arabic-eg' : 'eng-us';
+        return {
+            getLangValue: function (value) {
+                return res[value][currLang];
+            }
+        }
+    })
+})();
