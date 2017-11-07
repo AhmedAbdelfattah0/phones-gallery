48289139002f020efdda0d10cc8baeca151d0da3
AhmedAbdelfattah0 <33193362+AhmedAbdelfattah0@users.noreply.github.com>
2017-11-08 00:56:48 +0200

    create custom filter to search by min and max price

---
+++ phones/views/js/rangeFilter.js
@@ -0,0 +1,15 @@
+﻿(function () {
+
+    'use strict';
+    //custom filter to search by min and max price 
+    angular
+        .module('myApp')
+        .filter('range', function () {
+        return function (items, property, min, max) {
+            return items.filter(function (item) {
+                return item[property] >= min && item[property] <= max;
+            })
+        }
+    })
+
+})();
